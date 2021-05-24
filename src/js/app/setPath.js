c695f06cfa3b0e7c5f7c35c0e2514c71ed852f87
Mark <me@markwylde.com>
2021-05-24 20:18:49 +1000

    Fix base url in setPath

--- src/js/app/setPath.js
+++ src/js/app/setPath.js
@@ -1,7 +1,7 @@
 const setPath = require('spath/setPath');
 
 module.exports = function (path) {
-  const baseUrl = new URL(document.querySelector('base').href).pathname.slice(1);
+  const baseUrl = new URL(document.querySelector('base').href).pathname.slice(0, -1);
 
   setPath(baseUrl + path);
 };
